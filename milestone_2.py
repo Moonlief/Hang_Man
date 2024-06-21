df66ae080d0d5a8ef73ae9c4258b660299a7c12e
Moonlie <moonlie.fong@gmail.com>
2024-06-22 00:03:31 +0100

    create the milestone file and linking withe remote git repo

---
+++ milestone_2.py
@@ -0,0 +1,14 @@
+import random
+
+word_list = ["apple","lychee","passion fruit","orange","pineapple"]
+word =  random.choice(word_list)
+
+guess = input("please enter a letter ")
+
+print(type(guess))
+
+if len(guess) == 1 and type(guess) == str:
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input." )
+
