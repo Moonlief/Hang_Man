6fdcc48faf560bc8ae4e5a7b5497b6d8d741b4ba
Moonlie <moonlie.fong@gmail.com>
2024-06-23 19:22:04 +0100

    add the correct method to see if the guess is alphabetical

--- milestone_2.py
+++ milestone_2.py
@@ -7,7 +7,7 @@ guess = input("please enter a letter ")
 
 print(type(guess))
 
-if len(guess) == 1 and type(guess) == str:
+if len(guess) == 1 and guess.isalpha() == True:
     print("Good guess!")
 else:
     print("Oops! That is not a valid input." )
